a29189093ff9bee9e8c10288566fd9d27a6c8010
Your Name <you@example.com>
2026-01-02 06:42:46 +0100

    Cpp05/ex00 finished

--- Module05/ex00/includes/Bureaucrat.hpp
+++ Module05/ex00/includes/Bureaucrat.hpp
@@ -6,7 +6,46 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 04:06:14 by fracurul          #+#    #+#             */
-/*   Updated: 2026/01/02 04:06:15 by fracurul         ###   ########.fr       */
+/*   Updated: 2026/01/02 05:56:47 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+#include <string>
+#include <iostream>
+#include <exception>
+
+class	Bureaucrat
+{
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& copy);
+		Bureaucrat&	operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char*	what() const throw();
+		};
+
+		const std::string&		getName() const;
+		int						getGrade() const;
+		void					incrementGrade();
+		void					decrementGrade();
+	private:
+		const std::string		_name;
+		int						_grade;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+
+#endif
--- Module05/ex00/src/Bureaucrat.cpp
+++ Module05/ex00/src/Bureaucrat.cpp
@@ -6,7 +6,86 @@
 /*   By: fracurul <fracurul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/02 04:06:34 by fracurul          #+#    #+#             */
-/*   Updated: 2026/01/02 04:06:35 by fracurul         ###   ########.fr       */
+/*   Updated: 2026/01/02 06:40:44 by fracurul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Bureaucrat.hpp"
+
+// Constructor por defecto
+Bureaucrat::Bureaucrat() : _name("Default"), _grade(150)
+{
+}
+
+// Constructor parametrizado
+Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+// Constructor de copia
+Bureaucrat::Bureaucrat(const Bureaucrat& copy) : _name(copy._name), _grade(copy._grade)
+{
+}
+
+// Operador de asignación
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+		this->_grade = other._grade;
+	return *this;
+}
+
+// Destructor
+Bureaucrat::~Bureaucrat()
+{
+}
+
+// Getter para nombre
+const std::string& Bureaucrat::getName() const
+{
+	return this->_name;
+}
+
+// Getter para grade
+int Bureaucrat::getGrade() const
+{
+	return this->_grade;
+}
+
+// Incrementa grade (grado 1 es el más alto, así que incrementar = decrementar el número)
+void Bureaucrat::incrementGrade()
+{
+	if (this->_grade - 1 < 1)
+		throw GradeTooHighException();
+	this->_grade--;
+}
+
+// Decrementa grade (grado 150 es el más bajo, así que decrementar = incrementar el número)
+void Bureaucrat::decrementGrade()
+{
+	if (this->_grade + 1 > 150)
+		throw GradeTooLowException();
+	this->_grade++;
+}
+
+// Implementación del what() para las excepciones
+const char*	Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high!");
+}
+
+const char*	Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low!");
+}
+
+// Sobrecarga del operator<<
+std::ostream&	operator<<(std::ostream& os, const Bureaucrat& bureaucrat)
+{
+	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (os);
+}
