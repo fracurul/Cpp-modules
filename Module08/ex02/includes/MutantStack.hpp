c166136758d9957e36076774fc30366ba120a65c
Your Name <you@example.com>
2026-05-04 16:46:59 +0200

    hpp fixed tabs

--- Module08/ex02/includes/MutantStack.hpp
+++ Module08/ex02/includes/MutantStack.hpp
@@ -12,7 +12,7 @@ class	MutantStack : public std::stack<T>
 		MutantStack& operator=(const MutantStack& copy);
 		~MutantStack();
 
-		typedef typename std::stack<T>::container_type::iterator	it;
+		typedef typename std::stack<T>::container_type::iterator		it;
 		typedef typename std::stack<T>::container_type::const_iterator	constIt;
 
 		it		begin();
